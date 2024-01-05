2f9e9cfcdc548956766de98a7b15c5431c792626
松崎好浩 <y.matsuzaki@ris.co.jp>
2024-01-05 16:35:55 +0900

    更新

--- sample/script/三角関数/三角比.js
+++ sample/script/三角関数/三角比.js
@@ -3,13 +3,15 @@
 
 const AXIZ_COLOR = Drawer.BLACK;
 const MEASURE_COLOR = Drawer.BLACK;
+const RULER_COLOR = Drawer.GRAY;
 const CIRCLE_COLOR = Drawer.BLACK;
-const COS_COLOR = Drawer.BLACK;
-const SIN_COLOR = Drawer.BLACK;
+const KNOB_COLOR = Drawer.GREEN;
+const COS_COLOR = Drawer.BLUE;
+const SIN_COLOR = Drawer.RED;
 const TAN_COLOR = Drawer.BLACK;
-const UNIT_RADIUS = 80;
+const UNIT_RADIUS = 60;
 const WAVE_LENGTH = 240;
-const GAP = 50;
+const GAP = 40;
 
 /** @type{LineInfo[]} */
 let gLineList = [];
@@ -57,13 +59,13 @@ function init() {
     gDrawer.Offset = new vec(gWaveBegin - GAP + 5, gDrawer.Height / 4 + 40);
 
     gLineList.push(new LineInfo(
-        -gCircleRadius, 0,
+        -gCircleRadius - 10, 0,
         gWaveBegin + WAVE_LENGTH, 0,
         0.5, AXIZ_COLOR
     ));
     gLineList.push(new LineInfo(
         0, -gWaveBegin - WAVE_LENGTH,
-        0, gCircleRadius,
+        0, gCircleRadius + 10,
         0.5, AXIZ_COLOR
     ));
     for (let deg=0; deg<=360; deg += 15) {
@@ -106,9 +108,14 @@ function init() {
     }
 
     gLineList.push(new LineInfo(
+        UNIT_RADIUS, -gDrawer.Height,
+        UNIT_RADIUS, gDrawer.Height,
+        0.5, TAN_COLOR, true
+    ));
+    gLineList.push(new LineInfo(
         gCircleRadius, -gDrawer.Height,
         gCircleRadius, gDrawer.Height,
-        1, TAN_COLOR, true
+        1, TAN_COLOR
     ));
 
     let tanList = [];
@@ -124,7 +131,7 @@ function init() {
             }
             continue;
         }
-        tanList.push(new vec(x, t * gCircleRadius));
+        tanList.push(new vec(x, t * UNIT_RADIUS));
     }
     if (0 < tanList.length) {
         gTanLines.push(tanList);
@@ -137,20 +144,12 @@ function main() {
     for (let i=0; i<gLineList.length; i++) {
         gLineList[i].draw(gDrawer);
     }
-    for (let i=0; i<gLabelList.length; i++) {
-        let lbl = gLabelList[i];
-        if (lbl.center) {
-            gDrawer.drawStringC(lbl.pos, lbl.text, 14);
-        } else {
-            gDrawer.drawString(lbl.pos, lbl.text, 14);
-        }
-    }
-
-    gDrawer.drawCircle(new vec(), gCircleRadius, CIRCLE_COLOR, 0.5);
+    gDrawer.drawCircleD(new vec(), UNIT_RADIUS, CIRCLE_COLOR, 0.5);
+    gDrawer.drawCircle(new vec(), gCircleRadius, CIRCLE_COLOR);
 
     for (let i=0; i<gTanLines.length; i++) {
         let tanLine = gTanLines[i];
-        gDrawer.drawPolylineD(tanLine, TAN_COLOR, 1);
+        gDrawer.drawPolyline(tanLine, TAN_COLOR, 1);
     }
     gDrawer.drawPolyline(gRCosLine, COS_COLOR, 0.5);
     gDrawer.drawPolyline(gRSinLine, SIN_COLOR, 0.5);
@@ -165,9 +164,10 @@ function main() {
     let x = gWaveBegin + gTheta * WAVE_LENGTH / Math.PI / 2;
     let vx = new vec(x, 0);
     let vy = new vec(0, -x);
-    let vzero = new vec();
+    let vo = new vec();
     let vp = new vec(Math.cos(gTheta) * gCircleRadius, Math.sin(gTheta) * gCircleRadius);
-    let vt = new vec(gCircleRadius, Math.tan(gTheta) * gCircleRadius);
+    let vt = new vec(UNIT_RADIUS, UNIT_RADIUS * Math.tan(gTheta));
+    let vrt = new vec(gCircleRadius, gCircleRadius * Math.tan(gTheta));
     let vc = new vec(vp.X, 0);
     let wave_c = new vec(vp.X, -x);
     let wave_s = new vec(x, vp.Y);
@@ -179,39 +179,52 @@ function main() {
     }
     gDrawer.drawArc(new vec(), 18, 0, dangle, CIRCLE_COLOR, 3);
 
-    gDrawer.drawLine(vy, wave_c);
-    gDrawer.drawLine(vx, wave_s);
-    gDrawer.drawLine(vx, wave_t);
-    gDrawer.drawLine(vp, wave_c, COS_COLOR, 0.5);
-    gDrawer.drawLine(vp, wave_s, SIN_COLOR, 0.5);
-    gDrawer.drawLine(vt, wave_t, TAN_COLOR, 0.5);
-    gDrawer.drawLine(vzero, vt, TAN_COLOR, 1);
-    gDrawer.drawLine(vzero, vc, COS_COLOR, 1, 4);
+    gDrawer.drawLine(vx, wave_t, RULER_COLOR);
+    gDrawer.drawLine(vy, wave_c, RULER_COLOR);
+    gDrawer.drawLine(vx, wave_s, RULER_COLOR);
+    gDrawer.drawLine(vt, wave_t, TAN_COLOR);
+    gDrawer.drawLine(vp, wave_c, COS_COLOR);
+    gDrawer.drawLine(vp, wave_s, SIN_COLOR);
+    gDrawer.drawLine(vo, vrt, TAN_COLOR, 1);
+    gDrawer.drawLine(vo, vt, TAN_COLOR, 1);
+    gDrawer.drawLine(vo, vc, COS_COLOR, 1, 4);
     gDrawer.drawLine(vc, vp, SIN_COLOR, 1, 4);
-    gDrawer.drawLine(vzero, vp, CIRCLE_COLOR, 1, 4);
-    gDrawer.fillCircle(vp, 5, CIRCLE_COLOR);
+    gDrawer.drawLine(vo, vp, KNOB_COLOR, 1, 4);
+    gDrawer.fillCircle(vrt, 3, TAN_COLOR);
     gDrawer.fillCircle(vt, 5, TAN_COLOR);
+    gDrawer.fillCircle(vp, 5, KNOB_COLOR);
+    gDrawer.fillCircle(wave_t, 3, TAN_COLOR);
     gDrawer.fillCircle(wave_c, 3, COS_COLOR);
     gDrawer.fillCircle(wave_s, 3, SIN_COLOR);
-    gDrawer.fillCircle(wave_t, 3, TAN_COLOR);
 
+    for (let i=0; i<gLabelList.length; i++) {
+        let lbl = gLabelList[i];
+        if (lbl.center) {
+            gDrawer.drawStringC(lbl.pos, lbl.text, 14);
+        } else {
+            gDrawer.drawString(lbl.pos, lbl.text, 14);
+        }
+    }
+
+    let lblrT = new vec(vrt.X - 12, vrt.Y + 6);
+    let lblT = new vec(vt.X - 6, vt.Y + 6);
     let lblP = new vec(vp.X - 6, vp.Y + 6);
-    let lblT = new vec(gCircleRadius - 6, wave_t.Y + 6);
     let lblR = new vec(vp.X * 0.5 - 5, vp.Y * 0.5 + 5);
     let lblX = new vec(vc.X * 0.5, -5);
     let lblY = new vec(vp.X + 3, vp.Y * 0.5 - 3);
+    let lblRT = new vec(wave_t.X - 40, wave_t.Y + 3);
     let lblRC = new vec(wave_c.X + 2, wave_c.Y - 11);
-    let lblRS = new vec(wave_s.X - 54, wave_s.Y + 3);
-    let lblRT = new vec(wave_t.X - 56, wave_t.Y + 3);
-    gDrawer.drawStringXY(17, 3, "θ", 20);
-    gDrawer.drawString(lblP, "P", 20);
-    gDrawer.drawString(lblT, "T", 20);
-    gDrawer.drawString(lblR, "r", 24);
-    gDrawer.drawStringC(lblX, "x", 24);
-    gDrawer.drawString(lblY, "y", 24);
-    gDrawer.drawString(lblRC, "r cosθ", 20);
-    gDrawer.drawString(lblRS, "r sinθ", 20);
-    gDrawer.drawString(lblRT, "r tanθ", 20);
+    let lblRS = new vec(wave_s.X - 44, wave_s.Y + 3);
+    gDrawer.drawStringXY(17, 3, "θ", 18);
+    gDrawer.drawString(lblrT, "rT", 18);
+    gDrawer.drawString(lblT, "T", 18);
+    gDrawer.drawString(lblP, "P", 18);
+    gDrawer.drawString(lblR, "r", 20);
+    gDrawer.drawStringC(lblX, "x", 20);
+    gDrawer.drawString(lblY, "y", 20);
+    gDrawer.drawString(lblRT, "tanθ", 18);
+    gDrawer.drawString(lblRC, "r cosθ", 18);
+    gDrawer.drawString(lblRS, "r sinθ", 18);
 
     var tan = round1d(Math.sin(gTheta) / Math.cos(gTheta), 1, 3);
     document.getElementById("lblR").innerHTML = gRadius;
