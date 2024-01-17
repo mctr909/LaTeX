5c8c828f6c451f1b0e5f5ec4fb0bf433e6a2cff8
松崎好浩 <y.matsuzaki@ris.co.jp>
2024-01-17 18:08:45 +0900

    更新

--- sample/script/三角関数/正弦定理.js
+++ sample/script/三角関数/正弦定理.js
@@ -7,12 +7,12 @@ const AUX_COLOR = Color.GREEN;
 
 let gDrawer = new Drawer("disp", 400, 400);
 
-let gO = new vec(UNIT * 0.0, UNIT * 0.7);
 let gA = new vec(UNIT * -0.9, UNIT * -0.6);
-let gB = new vec(UNIT * 0.8, UNIT * -0.3);
-let gPoDrag = false;
+let gB = new vec(UNIT * 0.0, UNIT * 0.7);
+let gG = new vec(UNIT * 0.8, UNIT * -0.3);
 let gPaDrag = false;
 let gPbDrag = false;
+let gPgDrag = false;
 
 init();
 requestNextAnimationFrame(main);
@@ -25,99 +25,97 @@ function main() {
     gDrawer.clear();
 
     if (gDrawer.isDrag) {
-        if (!gPaDrag && !gPbDrag && distance(gDrawer.cursor, gO) <= 10) {
-            gPoDrag = true;
-        } else if (!gPoDrag && !gPbDrag && distance(gDrawer.cursor, gA) <= 10) {
+        if (!gPbDrag && !gPgDrag && distance(gDrawer.cursor, gA) <= 10) {
             gPaDrag = true;
-        } else if (!gPoDrag && !gPaDrag && distance(gDrawer.cursor, gB) <= 10) {
+        } else if (!gPaDrag && !gPgDrag && distance(gDrawer.cursor, gB) <= 10) {
             gPbDrag = true;
+        } else if (!gPaDrag && !gPbDrag && distance(gDrawer.cursor, gG) <= 10) {
+            gPgDrag = true;
         }
     } else {
-        gPoDrag = false;
         gPaDrag = false;
         gPbDrag = false;
+        gPgDrag = false;
     }
 
-    if (gPoDrag) {
-        gDrawer.cursor.copy(gO);
-    }
     if (gPaDrag) {
         gDrawer.cursor.copy(gA);
     }
     if (gPbDrag) {
         gDrawer.cursor.copy(gB);
     }
-
-    let ao = new vec();
-    let ab = new vec();
-    let bo = new vec();
-    gO.sub(gA, ao);
-    gB.sub(gA, ab);
-    gO.sub(gB, bo);
-    let angleA = ao.arg - ab.arg;
-    if (2*Math.PI < angleA) {
-        angleA -= 2*Math.PI;
-    }
-    if (angleA < 0) {
-        angleA += 2*Math.PI;
-    }
-    let angleB = Math.PI - (bo.arg - ab.arg);
-    if (2*Math.PI < angleB) {
-        angleB -= 2*Math.PI;
-    }
-    if (angleB < 0) {
-        angleB += 2*Math.PI;
-    }
-    let angleC = Math.PI - (angleA + angleB);
-    if (2*Math.PI < angleC) {
-        angleC -= 2*Math.PI;
-    }
-    if (angleC < 0) {
-        angleC += 4*Math.PI;
+    if (gPgDrag) {
+        gDrawer.cursor.copy(gG);
     }
 
-    let obL2 = ab.X*ab.X + ab.Y*ab.Y;
-    let k = (ab.X * ao.X + ab.Y*ao.Y) / obL2;
-    let c = new vec(ab.X * k + gA.X, ab.Y * k + gA.Y);
+    let ag = new vec();
+    let ba = new vec();
+    let gb = new vec();
+    gG.sub(gA, ag);
+    gA.sub(gB, ba);
+    gB.sub(gG, gb);
+    let abM = new vec((gA.X + gB.X)*0.5, (gA.Y + gB.Y)*0.5);
+    let agM = new vec((gA.X + gG.X)*0.5, (gA.Y + gG.Y)*0.5);
+    let gbM = new vec((gG.X + gB.X)*0.5, (gG.Y + gB.Y)*0.5);
+    let abC = new vec(abM.X - ba.Y, abM.Y + ba.X);
+    let agC = new vec(agM.X - ag.Y, agM.Y + ag.X);
+    let gbC = new vec(gbM.X - gb.Y, gbM.Y + gb.X);
+    gDrawer.drawLine(abM, abC, Color.GRAY75);
+    gDrawer.drawLine(agM, agC, Color.GRAY75);
+    gDrawer.drawLine(gbM, gbC, Color.GRAY75);
 
-    gDrawer.drawArc(gA, 20, ab.arg, angleA + ab.arg, Color.BLACK, 3);
-    gDrawer.drawArc(gB, 20, ab.arg - Math.PI - angleB, ab.arg - Math.PI, Color.BLACK, 3);
-    gDrawer.drawArc(gO, 20, ao.arg - Math.PI, bo.arg - Math.PI, Color.BLACK, 3);
+    let angleA = angleLimit(ba.arg - ag.arg - Math.PI);
+    let angleG = angleLimit(ag.arg - gb.arg + Math.PI);
+    let angleB = angleLimit(gb.arg - ba.arg - Math.PI);
 
-    gDrawer.drawLine(gA, gO);
+    let agL2 = ag.X*ag.X + ag.Y*ag.Y;
+    let k = (ag.X * ba.X + ag.Y*ba.Y) / agL2;
+    let c = new vec(ag.X * k + gA.X, ag.Y * k + gA.Y);
+
+    gDrawer.drawArc(gA, 20, ag.arg, ag.arg + angleA, Color.BLACK, 3);
+    gDrawer.drawArc(gB, 20, ba.arg, ba.arg + angleB, Color.BLACK, 3);
+    gDrawer.drawArc(gG, 20, ag.arg - angleG - Math.PI, ag.arg - Math.PI, Color.BLACK, 3);
+
     gDrawer.drawLine(gA, gB);
-    gDrawer.drawLine(gB, gO);
-    gDrawer.fillCircle(gO, 5);
-    gDrawer.fillCircle(gB, 5);
+    gDrawer.drawLine(gA, gG);
+    gDrawer.drawLine(gG, gB);
     gDrawer.fillCircle(gA, 5);
+    gDrawer.fillCircle(gB, 5);
+    gDrawer.fillCircle(gG, 5);
 
-    gDrawer.drawStringH(gA, gB, "α", 24, TEXT_COLOR, new vec(27,4,0));
-    gDrawer.drawStringH(gA, gB, "β", 24, TEXT_COLOR, new vec(-27,4,1));
-    gDrawer.drawStringV(c, gO, "θ", 24, TEXT_COLOR, new vec(0,-32,1));
-    gDrawer.drawStringH(gA, gB, "A", 24, TEXT_COLOR, new vec(-13,-8,0));
-    gDrawer.drawStringH(gA, gB, "B", 24, TEXT_COLOR, new vec(12,-8,1));
-    gDrawer.drawStringV(c, gO, "O", 24, TEXT_COLOR, new vec(0,12,1));
-    gDrawer.drawStringH(gA, gO, "a", 24, TEXT_COLOR, new vec(0,4,0.5));
-    gDrawer.drawStringV(gB, gO, "b", 24, TEXT_COLOR, new vec(8,0,0.5));
-    gDrawer.drawStringH(gA, gB, "c", 24, TEXT_COLOR, new vec(0,-15,0.5));
+    gDrawer.drawStringH(gA, gG, "α", 24, TEXT_COLOR, new vec(28,2,0));
+    gDrawer.drawStringH(gB, gA, "β", 24, TEXT_COLOR, new vec(27,4,0));
+    gDrawer.drawStringH(gG, gB, "γ", 24, TEXT_COLOR, new vec(27,4,0));
+    gDrawer.drawStringH(gA, gG, "A", 24, TEXT_COLOR, new vec(-13,-8,0));
+    gDrawer.drawStringV(c, gB, "B", 24, TEXT_COLOR, new vec(0,12,1));
+    gDrawer.drawStringH(gA, gG, "G", 24, TEXT_COLOR, new vec(12,-8,1));
+    gDrawer.drawStringV(gG, gB, "a", 24, TEXT_COLOR, new vec(8,0,0.5));
+    gDrawer.drawStringH(gA, gG, "b", 24, TEXT_COLOR, new vec(0,-15,0.5));
+    gDrawer.drawStringH(gA, gB, "g", 24, TEXT_COLOR, new vec(0,4,0.5));
 
-    let ta = ao.abs / UNIT;
-    let tb = bo.abs / UNIT;
-    let tc = ab.abs / UNIT;
+    let ta = gb.abs / UNIT;
+    let tb = ag.abs / UNIT;
+    let tg = ba.abs / UNIT;
     document.getElementById("lblA").innerHTML = round1d(ta);
     document.getElementById("lblB").innerHTML = round1d(tb);
-    document.getElementById("lblC").innerHTML = round1d(tc);
+    document.getElementById("lblG").innerHTML = round1d(tg);
     document.getElementById("lblAlpha").innerHTML
         = toFrac(angleA / Math.PI, "π", false)
         + "(" + round1d(angleA, 180 / Math.PI, 1) + "°)";
     document.getElementById("lblBeta").innerHTML
         = toFrac(angleB / Math.PI, "π", false)
         + "(" + round1d(angleB, 180 / Math.PI, 1) + "°)";
-    document.getElementById("lblTheta").innerHTML
-        = toFrac(angleC / Math.PI, "π", false)
-        + "(" + round1d(angleC, 180 / Math.PI, 1) + "°)";
+    document.getElementById("lblGamma").innerHTML
+        = toFrac(angleG / Math.PI, "π", false)
+        + "(" + round1d(angleG, 180 / Math.PI, 1) + "°)";
     document.getElementById("lblSAlpha").innerHTML = round1d(Math.sin(angleA));
     document.getElementById("lblSBeta").innerHTML = round1d(Math.sin(angleB));
-    document.getElementById("lblSTheta").innerHTML = round1d(Math.sin(angleC));
+    document.getElementById("lblSGamma").innerHTML = round1d(Math.sin(angleG));
+
     requestNextAnimationFrame(main);
+}
+
+function angleLimit(v) {
+    let n = parseInt(v / (2*Math.PI));
+    return v - 2*Math.PI*n;
 }
